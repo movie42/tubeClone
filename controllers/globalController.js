b49eca7d3774ba3e8c4af6cdcafbf15f070a2916
HYUNSU <movie3342@gmail.com>
2021-04-07 16:28:09 +0900

    text

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -2,3 +2,5 @@ export const home = (req, res) => res.render("home", { pageTitle: "메인" });
 export const login = (req, res) => res.render("login");
 export const logout = (req, res) => res.render("logout");
 export const join = (req, res) => res.render("join");
+
+("test");
